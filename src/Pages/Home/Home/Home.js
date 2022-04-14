5ac3b1406576eb7190ab8de48ea677272fb7b6e3
UttamSaha <uttamsaha982@gmail.com>
2022-04-14 21:45:01 +0600

    data displayed

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Services from "../Services/Services";
+
+const Home = () => {
+    return (
+        <div>
+            <Services></Services>
+        </div>
+    )
+}
+export default Home;
