ebf959da5355fe2b84b2a184768b961c46b44a44
UttamSaha <uttamsaha982@gmail.com>
2022-04-14 20:01:02 +0600

    components & router setup

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+      <footer>
+          <p><small>Copyright @ //dynamic year//</small></p>
+      </footer>
+  )
+};
+export default Footer;
