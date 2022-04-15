c3cb550971c5fbb0f38276b43bbf8c2d0a673e49
UttamSaha <uttamsaha982@gmail.com>
2022-04-15 20:23:58 +0600

    conditional css added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -6,6 +6,7 @@ import auth from "../../../firebase.init";
 import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Register = () => {
+  const [agree, setAgree] = useState(false);
 const [
   createUserWithEmailAndPassword,
   user,
@@ -28,7 +29,13 @@ const handleRegister = event => {
   const name = event.target.name.value;
   const email = event.target.email.value;
   const password = event.target.password.value;
-  createUserWithEmailAndPassword(email,password);
+  // const agree = event.target.terms.checked;
+  if(agree){
+    createUserWithEmailAndPassword(email,password);
+  }
+
+
+  
 }
   return (
     <div className="register-form">
@@ -37,9 +44,9 @@ const handleRegister = event => {
             <input className="form-control my-2" type="text" name="name" id="" required placeholder="Your Name: "/>
             <input className="form-control my-2" type="email" name="email" id="" required placeholder="Email.."/>
             <input className="form-control my-2" type="password" name="password" id="" required placeholder="password.."/>
-            <input type="checkbox" name="terms" id="terms"></input>
-            <label htmlFor="terms">Accept Terms & Conditions</label> <br></br>
-            <input className="btn btn-danger w-25 mt-3 mx-auto d-block" type="submit" value="Register" />
+            <input onClick={()=>setAgree(!agree)} type="checkbox" name="terms" id="terms"></input>
+            <label className={agree ? 'ps-2 text-primary' : 'ps-2 text-danger'} htmlFor="terms">Accept Terms & Conditions</label> <br></br>
+            <input disabled={!agree} className="btn btn-danger w-25 mt-3 mx-auto d-block" type="submit" value="Register" />
 
       </form>
       <p className="text-center">Already have an account? <span onClick={toggleReg} className="text-danger">Login</span></p>
