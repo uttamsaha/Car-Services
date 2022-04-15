0d5996b1fdbd3ff3ddf7e917f1ca4ad5d85fc69a
UttamSaha <uttamsaha982@gmail.com>
2022-04-15 19:06:02 +0600

    gogole sigin in done

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,23 +2,45 @@ import React from "react";
 import google from '../../../images/social/google.png'
 import facebook from '../../../images/social/facebook.png'
 import github from '../../../images/social/github.png'
+import auth from "../../../firebase.init";
+import { useSignInWithGithub, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 
 const SocialLogin = () => {
+    let errorElement;
+    const navigate = useNavigate()
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGithub, user, loading, error] = useSignInWithGithub(auth);
+    if (error) {
+        errorElement =  (
+          <div>
+            <p>Error: {error.message}</p>
+          </div>
+        );
+      }
+      
+
+      if(user){
+          navigate('/home');
+      }
+
+    
+    
     return (
-        <div>
+        <div className="d-block mx-auto">
             <div className="d-flex align-items-center">
                 <div style={{height:'1px'}} className="bg-danger w-50"></div>
                 <p className="mt-2 px-2">or</p>
                 <div style={{height:'1px'}} className="bg-danger w-50"></div>
             </div>
-
+            {errorElement}
             <div>
-                <button className="btn btn-warning w-50 d-flex align-items-center justify-content-center mx-auto my-2">
+                <button onClick={()=> signInWithGoogle()}  className="btn btn-warning w-50 d-flex align-items-center justify-content-center mx-auto my-2">
                     <img width={30} src={google} alt="" />
                     <span className="px-2">Google Sign In</span>
                 </button>
-                <button className="btn btn-warning w-50 d-flex align-items-center justify-content-center mx-auto m-2">
+                <button onClick={()=> signInWithGithub()} className="btn btn-warning w-50 d-flex align-items-center justify-content-center mx-auto m-2">
                     <img width={30} src={github} alt="" />
                     <span className="px-2">Facebook Sign In</span>
                 </button>
