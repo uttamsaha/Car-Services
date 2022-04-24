066ed4fd2e2dd01e58f823342b71db7ceaa6ec72
UttamSaha <uttamsaha982@gmail.com>
2022-04-24 15:57:11 +0600

    react hooks form added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Pages/About/About";
+import AddService from "./Pages/AddService/AddService";
 import Checkout from "./Pages/Checkout/Checkout";
 import Home from "./Pages/Home/Home/Home";
 import Login from "./Pages/Login/Login/Login";
@@ -33,6 +34,9 @@ function App() {
             </RequireAuth>
           }
         ></Route>
+        <Route path="/addservice" element={<RequireAuth>
+          <AddService></AddService>
+        </RequireAuth>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
