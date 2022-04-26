072d52af229c280510c2abe87a002edcf93c42d4
uttamsaha <uttamsaha982@gmail.com>
2022-04-26 11:21:03 +0600

    service hooks added

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,7 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import useServiceDetail from "../Hooks/useServiceDetail";
 
 const Checkout = () => {
+  const {serviceId} = useParams();
+  const [service] = useServiceDetail(serviceId);
   return (
     <div>
       <h2>Please Checkout</h2>
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import useServiceDetail from "../Hooks/useServiceDetail";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
-  const [service, setService] = useState({});
-  useEffect(()=>{
-    const url = `http://localhost:5000/service/${serviceId}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setService(data))
-  },[])
+  const [service] = useServiceDetail(serviceId);
   return (
     <div>
       <h2>You are about to book: {service.name}</h2>
-      <Link to="/checkout">
+      <Link to={`/checkout/${serviceId}`}>
         <button className="btn btn-danger">Proceed Checkout</button>
       </Link>
     </div>
